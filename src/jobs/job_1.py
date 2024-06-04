dd1bebfc052de898b4031c4f917f955cc302e1c0
phabrahao <44507879+phabrahao@users.noreply.github.com>
2024-06-03 21:28:03 -0300

    hw4

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,54 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    INSERT INTO
+    phabrahao.actors
+    WITH
+    last_year AS (
+        SELECT
+        *
+        FROM
+        phabrahao.actors
+        WHERE
+        current_year = 1929
+    ),
+    this_year AS (
+        SELECT
+        actor,
+        actor_id,
+        YEAR,
+        sum(votes * rating) / sum(votes) AS avg_rating,
+        array_agg(ROW(film, votes, rating, film_id)) AS film
+        FROM
+        bootcamp.actor_films
+        WHERE
+        YEAR = 1930
+        GROUP BY
+        actor,
+        actor_id,
+        YEAR
+    )
+    SELECT
+    COALESCE(ly.actor, ty.actor) AS actor,
+    COALESCE(ly.actor_id, ty.actor_id) AS actor_id,
+    CASE
+        WHEN ty.film IS NULL THEN ly.films
+        WHEN ty.film IS NOT NULL
+        AND ly.films IS NULL THEN ty.film
+        WHEN ty.film IS NOT NULL
+        AND ly.films IS NOT NULL THEN ty.film || ly.films
+    END AS films,
+    CASE
+        WHEN avg_rating > 8 THEN 'star'
+        WHEN avg_rating > 7 THEN 'good'
+        WHEN avg_rating > 6 THEN 'average'
+        WHEN avg_rating <= 6 THEN 'bad'
+    END AS quality_class,
+    ty.actor IS NOT NULL AS is_active,
+    COALESCE(ty.year, ly.current_year + 1) AS current_year
+    FROM
+    last_year ly
+    FULL OUTER JOIN this_year ty ON ly.actor_id = ty.actor_id
     """
     return query
 
@@ -14,7 +61,7 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "phabrahao.actors"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
--- src/jobs/job_2.py
+++ src/jobs/job_2.py
@@ -4,7 +4,35 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    INSERT INTO
+    phabrahao.user_devices_cumulated
+    WITH
+    new_data AS (
+        SELECT
+        user_id,
+        browser_type,
+        date(event_time) AS DATE
+        FROM
+        bootcamp.web_events we
+        LEFT JOIN bootcamp.devices d ON we.device_id = d.device_id
+        WHERE
+        date(event_time) = date('2021-03-29')
+        GROUP BY
+        1,2,3
+    )
+    SELECT
+    COALESCE(udc.user_id, dl.user_id) AS user_id,
+    COALESCE(udc.browser_type, dl.browser_type) AS browser_type,
+    CASE
+        WHEN udc.dates_active IS NOT NULL THEN ARRAY[dl.date] || udc.dates_active
+        ELSE ARRAY[dl.date]
+    END AS dates_active,
+    COALESCE(dl.date, DATE_ADD('day', 1, udc.date)) AS DATE
+    FROM
+    phabrahao.user_devices_cumulated udc
+    FULL OUTER JOIN new_data dl ON udc.user_id = dl.user_id
+    AND udc.browser_type = dl.browser_type
+    AND udc.date = date('2021-03-28')
     """
     return query
 
@@ -14,7 +42,7 @@ def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_2(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "phabrahao.user_devices_cumulated"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
